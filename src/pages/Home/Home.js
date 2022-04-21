6ee777d0e7ee40e61f8bbd953022659eac1215e0
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-04-20 20:22:30 -0400

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,46 @@
+// css
+import styled from "styled-components";
+import * as pallette from '../../styled/ThemeVariables.js';
 
+// router
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <>
-    </>
+    <StyledHome>
+      <div className="links">
+        <h4>Financial Calculators</h4>
+        <Link to="/mortgage">Mortgage Calculator</Link>
+        <Link to="/auto">Auto Loan Calculator</Link>
+        <Link to="/compound">Compound Interest Calculator</Link>
+      </div>
+    </StyledHome>
   )
 }
+
+const StyledHome = styled.section`
+min-height: 80vh;
+  .links {
+    display: flex;
+    flex-direction: column;
+    h4 {
+      margin: 10px 0;
+      font-size: 40px;
+      color: ${pallette.accentColor};
+      @media(max-width: 550px){
+        font-size: ${pallette.subtitle};
+      }
+    }
+    a {
+      margin: 10px 0;
+      font-size: ${pallette.subtitle};
+      color: white;
+      @media(max-width: 550px){
+        font-size: 20px;
+      }
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
